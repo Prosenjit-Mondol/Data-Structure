31d67fa6ad5be42e577aed46600a1af663c84e9d
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-10-31 03:12:01 +0600

    10/31/24

--- test.cpp
+++ test.cpp
@@ -1,59 +1,138 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to add an edge to the graph
-void addEdge(vector<vector<int>>& adjList, int u, int v) {
-    adjList[u].push_back(v);
-    adjList[v].push_back(u); // For undirected graph
+string postfix(string s)
+{
+   
+    string p;
+    stack<char> st;
+    st.push('(');
+     s.push_back(')');
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            p.push_back(' ');
+        }
+        else
+        {
+            if (s[i] >= 48 && s[i] <= 57)
+            {
+                p.push_back(s[i]);
+                while (s[i + 1] >= 48 && s[i + 1] <= 57)
+                {
+                    p.push_back(s[i + 1]);
+                    i++;
+                }
+            }
+            else if (s[i] == '(')
+            {
+                st.push('(');
+            }
+            else if (s[i] == ')')
+            {
+                while (st.top() != '(')
+                {
+                    p.push_back(st.top());
+                    p.push_back(' ');
+                    st.pop();
+                }
+                st.pop();
+            }
+            else
+            {
+                if (st.top() == '(' || s[i] == '^')
+                {
+                    st.push(s[i]);
+                }
+                else if (s[i] == '+' || s[i] == '-')
+                {
+                    p.push_back(st.top());
+                    st.pop();
+                    st.push(s[i]);
+                }
+                else
+                {
+                    if (st.top() == '+' || st.top() == '-')
+                    {
+                        st.push(s[i]);
+                    }
+                    else if (st.top() == '^' || st.top() == '*' || st.top() == '/')
+                    {
+                        p.push_back(st.top());
+                        st.pop();
+                        st.push(s[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    return p;
 }
 
-// Function to delete a node from the graph
-void deleteNode(vector<vector<int>>& adjList, int node) {
-    if (node >= adjList.size()) return; // Node out of bounds
+void posttovalue(string s)
+{
+    stack<int>st;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i]==' ')
+        {
+            continue;
+        }
+        else if (s[i]>=48&&s[i]<=57)
+        {
+            string ss;
+            ss.push_back(s[i]);
+            while (s[i+1]>=48&&s[i+1]<=57)
+            {
+                ss.push_back(s[i+1]);
+                i++;
+            }
+            int x=stoi(ss);
+            st.push(x);
+        }
+        else
+        {
+            int f=st.top();
+            st.pop();
+            int l=st.top();
+            st.pop();
 
-    // Remove all edges connected to the node
-    for (int neighbor : adjList[node]) {
-        adjList[neighbor].erase(remove(adjList[neighbor].begin(), adjList[neighbor].end(), node), adjList[neighbor].end());
+            int r;
+            if (s[i]=='+')
+            {
+                r=l+f;
+            }
+            if (s[i]=='-')
+            {
+                r=l-f;
+            }
+            if (s[i]=='*')
+            {
+                r=l*f;
+            }
+            if (s[i]=='/')
+            {
+                r=l/f;
+            }
+            st.push(r);
+        }
+        
     }
-
-    // Clear the list of the node itself
-    adjList[node].clear();
+    cout<<st.top()<<'\n';
 }
 
-int main() {
-    int nodes = 5; // Initial graph with 5 nodes (0 to 4)
-    vector<vector<int>> adjList(nodes);
+int main()
+{
+    string s;
+    getline(cin, s);
+    string p = postfix(s);
 
-    // Adding some edges
-    addEdge(adjList, 0, 1);
-    addEdge(adjList, 0, 2);
-    addEdge(adjList, 1, 3);
-    addEdge(adjList, 2, 4);
+    cout << p << "\n";
 
-    cout << "Graph before deleting a node:" << endl;
-    for (int i = 0; i < adjList.size(); ++i) {
-        cout << "Node " << i << ": ";
-        for (int neighbor : adjList[i]) {
-            cout << neighbor << " ";
-        }
-        cout << endl;
-    }
-
-    // Delete a node
-    int nodeToDelete = 1;
-    deleteNode(adjList, nodeToDelete);
-
-    cout << "\nGraph after deleting node " << nodeToDelete << ":" << endl;
-    for (int i = 0; i < adjList.size(); ++i) {
-        cout << "Node " << i << ": ";
-        for (int neighbor : adjList[i]) {
-            cout << neighbor << " ";
-        }
-        cout << endl;
-    }
+    posttovalue(p);
 
     return 0;
 }
