340e7469407688da5de6ef99a23190ec7a20f600
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-10-30 21:11:45 +0600

    10/30/24

--- test.cpp
+++ test.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    cout << "Enter the number of vertices: ";
-    int n; // Number of vertices
-    cin >> n;
-    cout << "Enter the number of edges: ";
-    int x; // Number of edges
-    cin >> x;
+const int MAX_NODES = 100;
+vector<int> adj[MAX_NODES];
 
-    vector<vector<int>> adj(n); // Adjacency list for n vertices
+void insertNode(int node) {
+    if (node >= MAX_NODES) return;
+    // No action needed here since we can directly add edges
+}
 
-    cout << "Enter the edges (pair of vertices for each edge):" << endl;
-    for (int i = 0; i < x; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
+void insertEdge(int u, int v) {
+    adj[u].push_back(v);
+}
+
+void deleteNode(int node) {
+    for (int i = 0; i < MAX_NODES; i++) {
+        adj[i].erase(remove(adj[i].begin(), adj[i].end(), node), adj[i].end());
     }
+    adj[node].clear();
+}
 
-    // Print adjacency list
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Node " << i << ": ";
-        for (int j = 0; j < adj[i].size(); j++)
-        {
-            cout << adj[i][j] << " ";
+void deleteEdge(int u, int v) {
+    adj[u].erase(remove(adj[u].begin(), adj[u].end(), v), adj[u].end());
+}
+
+void printGraph() {
+    for (int i = 0; i < MAX_NODES; i++) {
+        if (!adj[i].empty()) {
+            cout << i << " -> ";
+            for (int j : adj[i]) {
+                cout << j << " ";
+            }
+            cout << endl;
         }
-        cout << '\n';
     }
+}
+
+int main() {
+    insertNode(1);
+    insertNode(2);
+    insertNode(3);
+
+    insertEdge(1, 2);
+    insertEdge(2, 3);
+    insertEdge(1, 3);
+
+    cout << "Graph connections:\n";
+    printGraph();
+
+    deleteEdge(1, 3);
+    cout << "\nAfter deleting edge (1, 3):\n";
+    printGraph();
+
+    deleteNode(2);
+    cout << "\nAfter deleting node 2:\n";
+    printGraph();
 
     return 0;
 }
