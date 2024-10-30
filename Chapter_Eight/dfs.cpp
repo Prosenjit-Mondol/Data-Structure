81889de1de28c3cb5a162f44046eee708f144b00
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-10-30 20:18:44 +0600

    10/30/24

---
+++ Chapter_Eight/dfs.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void dfsre(vector<vector<int>>& adj,vector<bool>& visit,int s){
+    
+    visit[s]=true;
+    cout<<s<<" ";
+
+    for (int i: adj[s])
+    {
+        if (!visit[i])
+        {
+            dfsre(adj,visit,i);
+        }
+        
+    }
+    
+}
+
+
+void dfs(vector<vector<int>>& adj,int st){
+    vector<bool>visit(adj.size(),false);
+    dfsre(adj,visit,st);
+}
+
+
+int main()
+{
+    cout<<"Enter the number of vertexs: ";
+    int n;
+    cin>>n;
+    cout<<"Enter the number of edges: ";
+    int x;
+    cin>>x;
+    vector<vector<int>>adj(n);
+    for (int i = 0; i < x; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    cout<<"BFS starting node 0\n";
+    dfs(adj,0);
+    return 0;
+}
