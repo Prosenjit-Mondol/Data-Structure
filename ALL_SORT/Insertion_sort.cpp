3b028405e5f63f4bf096f0b3f90ad285128716c2
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-10-31 11:22:17 +0600

    10/31/24

---
+++ ALL_SORT/Insertion_sort.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion(vector<int>& v,int n)
+{
+    if (n==1)
+    {
+        return;
+    }
+
+
+    for (int i = 1; i < n; i++)
+    {
+        int j=i-1;
+        int k=v[i];
+        while (j>=0 && v[j]>k)
+        {
+            v[j+1]=v[j];
+            j--;
+            v[j+1]=k;
+        }
+        
+    }
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+
+    insertion(v,n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+    return 0;
+}
