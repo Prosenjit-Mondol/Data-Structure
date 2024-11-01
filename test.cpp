11e235c8e9d5331150eb281b1cfde6f0b4d822f1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-02 01:47:59 +0600

    11/02/24

--- test.cpp
+++ test.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+    queue<int> q;
+    for (int i = 0; i < 5; i++)
+    {
+        int a;
+        cin>>a;
+        q.push(a);
+    }
+    while(!q.empty())
+    {
+        cout<<q.front()<<" ";
+        q.pop();
+    }
     
+    return 0;
 }
