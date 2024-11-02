bcc301c5b9103b5aa4e8c0fed9471b48e97163fc
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-02 19:11:11 +0600

    11/02/24

--- Chapter Six/Quick_Sort.cpp
+++ Chapter Six/Quick_Sort.cpp
@@ -3,7 +3,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int partition(int v[], int s, int e)
 {
     int pivot = v[s];
@@ -21,7 +20,7 @@ int partition(int v[], int s, int e)
     swap(v[s], v[pivotI]);
 
     int i = s, j = e;
-    
+
     while (i < pivotI && j > pivotI)
     {
         while (v[i] <= pivot)
@@ -43,31 +42,27 @@ int partition(int v[], int s, int e)
     return pivotI;
 }
 
-
-void quickSort(int arr[], int low ,int high)
+void quickSort(int arr[], int low, int high)
 {
-    stack<pair<int,int>> s;
-    s.push({low,high});
+    stack<pair<int, int>> s;
+    s.push({low, high});
 
     while (!s.empty())
     {
-        int l=s.top().first;
-        int h=s.top().second;
+        int l = s.top().first;
+        int h = s.top().second;
 
         s.pop();
-        int p=partition(arr,low ,high);
-         if (p-1>l)
-         {
-            s.push({l,p-1});
-         }
-         if (p+1<h)
-         {
-            s.push({p+1,high});
-         }
-         
+        int p = partition(arr, low, high);
+        if (p - 1 > l)
+        {
+            s.push({l, p - 1});
+        }
+        if (p + 1 < h)
+        {
+            s.push({p + 1, high});
+        }
     }
-    
-
 }
 
 int main()
@@ -79,12 +74,11 @@ int main()
     {
         cin >> v[i];
     }
-    quickSort(v, 0, n-1);
+    quickSort(v, 0, n - 1);
 
     for (auto element : v)
     {
         cout << element << " ";
     }
-
     return 0;
 }
