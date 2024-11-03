f7ac929a69b1a6d44fd635ee9196eaa67ca84fb7
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-11-03 21:30:47 +0600

    11/3/24

--- test.cpp
+++ test.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int v[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j= 0; j <n-i; j++)
+        {
+            if (v[j]>v[j+1])
+            {
+                 swap(v[j],v[j+1]);
+            }
+            
+        }
+        
+    }
+    for(auto element : v)
+    {
+    cout<<element<<" ";
+    }
+    return 0;
+}
